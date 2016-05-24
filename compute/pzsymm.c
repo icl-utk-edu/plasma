f2a4c1de029c37a7daf45968f45c948555455bb9
Samuel Relton <samuel.relton@manchester.ac.uk>
2016-05-24 13:01:05 +0100

    Avoided casting for zone variables.

--- compute/pzsymm.c
+++ compute/pzsymm.c
@@ -40,7 +40,7 @@ void plasma_pzsymm(PLASMA_enum side, PLASMA_enum uplo,
     int tempmm, tempnn, tempkn, tempkm;
 
     PLASMA_Complex64_t zbeta;
-    PLASMA_Complex64_t zone = (PLASMA_Complex64_t)1.0;
+    PLASMA_Complex64_t zone = 1.0;
 
     if (sequence->status != PLASMA_SUCCESS)
         return;
