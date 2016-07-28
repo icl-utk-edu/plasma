50a20b565e0ee285e61e565f16e37bae38512ebf
Mark Gates <mgates3@icl.utk.edu>
2016-07-28 12:03:37 -0400

    reformat common comment for 80 characters

--- test/test_zsyrk.c
+++ test/test_zsyrk.c
@@ -41,9 +41,10 @@
  * @param[in]  param - array of parameters
  * @param[out] info  - string of column labels or column values; length InfoLen
  *
- * If param is NULL     and info is NULL,     print usage and return.
- * If param is NULL     and info is non-NULL, set info to column headings and return.
- * If param is non-NULL and info is non-NULL, set info to column values   and run test.
+ * If param is NULL and info is NULL,     print usage and return.
+ * If param is NULL and info is non-NULL, set info to column labels and return.
+ * If param is non-NULL and info is non-NULL, set info to column values
+ * and run test.
  ******************************************************************************/
 void test_zsyrk(param_value_t param[], char *info)
 {
