b5250b981c2d8ef090c8b2bf62a8a8a442cafca2
Maksims Abalenkovs <m.abalenkovs@manchester.ac.uk>
2016-10-10 17:24:34 +0100

    * Minor corrections were introduced to xTRADD related routines.

--- compute/pztradd.c
+++ compute/pztradd.c
@@ -58,7 +58,7 @@ void plasma_pztradd(plasma_enum_t uplo, plasma_enum_t transa,
                     int ldam = plasma_tile_mmain(A, m);
                     int ldbm = plasma_tile_mmain(B, m);
                     core_omp_zgeadd(
-                        transa, 
+                        transa,
                         mvbm, nvbn,
                         alpha, A(m, n), ldam,
                         beta,  B(m, n), ldbm);
