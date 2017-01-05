e065937214265749aa977d2a2028658212929f37
Mark Gates <mgates3@icl.utk.edu>
2017-01-05 15:37:47 -0500

    Add LAPACK_zlantr. Add const.

--- include/core_lapack_z.h
+++ include/core_lapack_z.h
@@ -19,23 +19,31 @@ extern "C" {
 
 // LAPACK_GLOBAL is Fortran name mangling macro from LAPACKE
 
-#ifndef LAPACK_zlassq
-#define LAPACK_zlassq LAPACK_GLOBAL(zlassq, ZLASSQ)
+// LAPACKE_zlantr broken (returns 0) in LAPACKE < 3.6.1
+#ifndef LAPACK_zlantr
+#define LAPACK_zlantr LAPACK_GLOBAL(zlantr, ZLANTR)
+double LAPACK_zlantr(const char *norm, const char *uplo, const char *diag,
+                     const int *m, const int *n,
+                     const plasma_complex64_t *A, const int *lda,
+                     double *work);
 #endif
-
-void LAPACK_zlassq(int *n, const plasma_complex64_t *x, int *incx,
-                   double *scale, double *sumsq);
 
 // LAPACKE_zlascl not available in LAPACKE < 3.6.0
 #ifndef LAPACK_zlascl
 #define LAPACK_zlascl LAPACK_GLOBAL(zlascl, ZLASCL)
+void LAPACK_zlascl(const char *type, const int *kl, const int *ku,
+                   const double *cfrom, const double *cto,
+                   const int *m, const int *n,
+                   plasma_complex64_t *A, const int *lda,
+                   int *info);
 #endif
 
-void LAPACK_zlascl(char* type, int* kl, int* ku,
-                   double* cfrom, double* cto,
-                   int* m, int* n,
-                   plasma_complex64_t* A, int* lda,
-                   int *info);
+// LAPACKE_zlassq not available yet
+#ifndef LAPACK_zlassq
+#define LAPACK_zlassq LAPACK_GLOBAL(zlassq, ZLASSQ)
+void LAPACK_zlassq(const int *n, const plasma_complex64_t *x, const int *incx,
+                   double *scale, double *sumsq);
+#endif
 
 #ifdef __cplusplus
 }  // extern "C"
