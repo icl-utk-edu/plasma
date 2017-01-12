ef90715206e591dbd0ae3c82784d46977614f88d
Ichitaro Yamazaki <ic.yamazaki@gmail.com>
2017-01-12 17:02:48 -0500

    test for zerocol in zgbsv.

--- test/test_zgbsv.c
+++ test/test_zgbsv.c
@@ -16,6 +16,7 @@
 #include "plasma.h"
 
 #include <assert.h>
+#include <math.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -55,11 +56,12 @@ void test_zgbsv(param_value_t param[], char *info)
             print_usage(PARAM_NB);
             print_usage(PARAM_IB);
             print_usage(PARAM_NTPF);
+            print_usage(PARAM_ZEROCOL);
         }
         else {
             // Return column labels.
             snprintf(info, InfoLen,
-                     "%*s %*s %*s %*s %*s %*s %*s %*s",
+                     "%*s %*s %*s %*s %*s %*s %*s %*s %*s",
                      InfoSpacing, "N",
                      InfoSpacing, "KL",
                      InfoSpacing, "KU",
@@ -67,13 +69,14 @@ void test_zgbsv(param_value_t param[], char *info)
                      InfoSpacing, "PadA",
                      InfoSpacing, "NB",
                      InfoSpacing, "IB",
-                     InfoSpacing, "NTPF");
+                     InfoSpacing, "NTPF",
+                     InfoSpacing, "ZeroCol");
         }
         return;
     }
     // Return column values.
     snprintf(info, InfoLen,
-             "%*d %*d %*d %*d %*d %*d %*d %*d",
+             "%*d %*d %*d %*d %*d %*d %*d %*d %*d",
              InfoSpacing, param[PARAM_N].i,
              InfoSpacing, param[PARAM_KL].i,
              InfoSpacing, param[PARAM_KU].i,
@@ -81,7 +84,8 @@ void test_zgbsv(param_value_t param[], char *info)
              InfoSpacing, param[PARAM_NRHS].i,
              InfoSpacing, param[PARAM_NB].i,
              InfoSpacing, param[PARAM_IB].i,
-             InfoSpacing, param[PARAM_NTPF].i);
+             InfoSpacing, param[PARAM_NTPF].i,
+             InfoSpacing, param[PARAM_ZEROCOL].i);
 
     //================================================================
     // Set parameters.
@@ -107,11 +111,11 @@ void test_zgbsv(param_value_t param[], char *info)
     //================================================================
     // Allocate and initialize arrays.
     //================================================================
-    plasma_complex64_t *A =
-        (plasma_complex64_t*)malloc((size_t)lda*n*sizeof(plasma_complex64_t));
+    plasma_complex64_t *A = (plasma_complex64_t*)malloc(
+        (size_t)lda*n*sizeof(plasma_complex64_t));
     assert(A != NULL);
-    plasma_complex64_t *X =
-        (plasma_complex64_t*)malloc((size_t)ldx*nrhs*sizeof(plasma_complex64_t));
+    plasma_complex64_t *X = (plasma_complex64_t*)malloc(
+        (size_t)ldx*nrhs*sizeof(plasma_complex64_t));
     assert(X != NULL);
     int *ipiv = (int*)malloc((size_t)n*sizeof(int));
     assert(ipiv != NULL);
@@ -124,7 +128,8 @@ void test_zgbsv(param_value_t param[], char *info)
     // copy X to B for test
     plasma_complex64_t *B = NULL;
     if (test) {
-        B = (plasma_complex64_t*)malloc((size_t)ldb*nrhs*sizeof(plasma_complex64_t));
+        B = (plasma_complex64_t*)malloc(
+            (size_t)ldb*nrhs*sizeof(plasma_complex64_t));
         assert(B != NULL);
         LAPACKE_zlacpy_work(LAPACK_COL_MAJOR, 'F', n, nrhs, X, ldx, B, ldb);
     }
@@ -139,6 +144,9 @@ void test_zgbsv(param_value_t param[], char *info)
     for (int j = 0; j < n; j++) {
         for (int i = j+kl+1; i < n; i++) A[i + j*lda] = 0.0;
     }
+    int zerocol = param[PARAM_ZEROCOL].i;
+    if (zerocol >= 0 && zerocol < n)
+        memset(&A[zerocol*lda], 0, n*sizeof(plasma_complex64_t));
 
     // save A for test
     plasma_complex64_t *Aref = NULL;
@@ -163,15 +171,25 @@ void test_zgbsv(param_value_t param[], char *info)
     for (int j = 0; j < n; j++) {
         int i_kl = imax(0,   j-ku);
         int i_ku = imin(n-1, j+kl);
-        for (int i = 0; i < ldab; i++) AB[i + j*ldab] = 0.0;
-        for (int i = i_kl; i <= i_ku; i++) AB[kl + i-(j-ku) + j*ldab] = A[i + j*lda];
+        for (int i = 0; i < ldab; i++)
+            AB[i + j*ldab] = 0.0;
+        for (int i = i_kl; i <= i_ku; i++) 
+            AB[kl + i-(j-ku) + j*ldab] = A[i + j*lda];
+    }
+    plasma_complex64_t *ABref = NULL;
+    if (test) {
+        ABref = (plasma_complex64_t*)malloc(
+            (size_t)ldab*n*sizeof(plasma_complex64_t));
+        assert(ABref != NULL);
+
+        memcpy(ABref, AB, (size_t)ldab*n*sizeof(plasma_complex64_t));
     }
 
     //================================================================
     // Run and time PLASMA.
     //================================================================
     plasma_time_t start = omp_get_wtime();
-    plasma_zgbsv(n, kl, ku, nrhs, AB, ldab, ipiv, X, ldx);
+    int plainfo = plasma_zgbsv(n, kl, ku, nrhs, AB, ldab, ipiv, X, ldx);
 
     plasma_time_t stop = omp_get_wtime();
     plasma_time_t time = stop-start;
@@ -183,31 +201,50 @@ void test_zgbsv(param_value_t param[], char *info)
     // Test results by comparing to a reference implementation.
     //================================================================
     if (test) {
-        // compute residual vector
-        plasma_complex64_t zone  =  1.0;
-        plasma_complex64_t zmone = -1.0;
-        cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, nrhs, n,
-                    CBLAS_SADDR(zmone), Aref, lda,
-                                        X, ldx,
-                    CBLAS_SADDR(zone),  B, ldb);
+        if (plainfo == 0) {
+            // compute residual vector
+            plasma_complex64_t zone  =  1.0;
+            plasma_complex64_t zmone = -1.0;
+            cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, nrhs, n,
+                        CBLAS_SADDR(zmone), Aref, lda,
+                                            X, ldx,
+                        CBLAS_SADDR(zone),  B, ldb);
 
-        // compute various norms
-        double *work = NULL;
-        work = (double*)malloc((size_t)n*sizeof(double));
-        assert(work != NULL);
+            // compute various norms
+            double *work = NULL;
+            work = (double*)malloc((size_t)n*sizeof(double));
+            assert(work != NULL);
 
-        double Anorm = LAPACKE_zlange_work(LAPACK_COL_MAJOR, 'F', n, n,    A, lda, work);
-        double Xnorm = LAPACKE_zlange_work(LAPACK_COL_MAJOR, 'I', n, nrhs, X, ldb, work);
-        double Rnorm = LAPACKE_zlange_work(LAPACK_COL_MAJOR, 'I', n, nrhs, B, ldb, work);
-        double residual = Rnorm/(n*Anorm*Xnorm);
+            double Anorm = LAPACKE_zlange_work(
+                LAPACK_COL_MAJOR, 'F', n, n,    A, lda, work);
+            double Xnorm = LAPACKE_zlange_work(
+                LAPACK_COL_MAJOR, 'I', n, nrhs, X, ldb, work);
+            double Rnorm = LAPACKE_zlange_work(
+                LAPACK_COL_MAJOR, 'I', n, nrhs, B, ldb, work);
+            double residual = Rnorm/(n*Anorm*Xnorm);
 
             param[PARAM_ERROR].d = residual;
             param[PARAM_SUCCESS].i = residual < tol;
 
-        // free workspaces
-        free(work);
-        free(X);
+            // free workspaces
+            free(work);
+        }
+        else {
+            int lapinfo = LAPACKE_zgbsv(
+                              LAPACK_COL_MAJOR,
+                              n, kl, ku, nrhs, ABref, ldab, ipiv, X, ldx);
+            if (plainfo == lapinfo) {
+                param[PARAM_ERROR].d = 0.0;
+                param[PARAM_SUCCESS].i = 1;
+            }
+            else {
+                param[PARAM_ERROR].d = INFINITY;
+                param[PARAM_SUCCESS].i = 0;
+            }
+        }
+        free(B);
         free(Aref);
+        free(ABref);
     }
 
     //================================================================
@@ -216,5 +253,5 @@ void test_zgbsv(param_value_t param[], char *info)
     free(A);
     free(AB);
     free(ipiv);
-    free(B);
+    free(X);
 }
