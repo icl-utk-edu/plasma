3216ec875c35434999a3fe3caf5e96eb3e33dec2
Panruo Wu <pwu11@icl.utk.edu>
2017-04-26 16:25:12 -0400

    include/core_lapack.h: cleanup

--- include/core_lapack.h
+++ include/core_lapack.h
@@ -22,16 +22,12 @@
     #define LAPACK_GLOBAL(lcname,UCNAME)  lcname##_
     #endif
 #elif defined(PLASMA_WITH_ESSL)
-    //#include <cblas.h>
-    //#include <lapacke.h>
-    // unchanged fortran routine symbol
+    // GCC + ESSL(BLAS) + LAPACKE/CBLAS from LAPACK
     #include <cblas.h>
     #include <lapacke.h>
     #ifndef LAPACK_GLOBAL
     #define LAPACK_GLOBAL(lcname,UCNAME) lcname##_
     #endif
-    //#include <essl.h>
-    //#define LAPACK_zlascl zlascl
 #else
     #include <cblas.h>
     #include <lapacke.h>
