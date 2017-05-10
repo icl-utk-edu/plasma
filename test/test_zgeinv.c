ad4735b7d4ca2d9c4194d21c45fda85c215f417a
Samuel Relton <samuel.relton@manchester.ac.uk>
2017-05-10 10:23:16 +0100

    Adding geinv testing.

---
+++ test/test_zgeinv.c
@@ -0,0 +1,172 @@
+/**
+ *
+ * @file
+ *
+ *  PLASMA is a software package provided by:
+ *  University of Tennessee, US,
+ *  University of Manchester, UK.
+ *
+ * @precisions normal z -> c d s
+ *
+ **/
+#include "test.h"
+#include "flops.h"
+#include "core_blas.h"
+#include "core_lapack.h"
+#include "plasma.h"
+
+#include <assert.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <omp.h>
+
+#define COMPLEX
+
+#define A(i_, j_) A[(i_) + (size_t)lda*(j_)]
+
+/***************************************************************************//**
+ *
+ * @brief Tests zgeinv.
+ *
+ * @param[in,out] param - array of parameters
+ * @param[in]     run - whether to run test
+ *
+ * Sets flags in param indicating which parameters are used.
+ * If run is true, also runs test and stores output parameters.
+ ******************************************************************************/
+void test_zgeinv(param_value_t param[], bool run)
+{
+    //================================================================
+    // Mark which parameters are used.
+    //================================================================
+    param[PARAM_DIM    ].used = PARAM_USE_M | PARAM_USE_N;
+    param[PARAM_PADA   ].used = true;
+    param[PARAM_NB     ].used = true;
+    param[PARAM_IB     ].used = true;
+    param[PARAM_MTPF   ].used = true;
+    param[PARAM_ZEROCOL].used = true;
+    if (! run)
+        return;
+
+    //================================================================
+    // Set parameters.
+    //================================================================
+	int m = param[PARAM_DIM].dim.m;
+    int n = param[PARAM_DIM].dim.n;
+    int lda = imax(1, n + param[PARAM_PADA].i);
+
+    int test = param[PARAM_TEST].c == 'y';
+    double tol = param[PARAM_TOL].d * LAPACKE_dlamch('E');
+
+    //================================================================
+    // Set tuning parameters.
+    //================================================================
+    plasma_set(PlasmaTuning, PlasmaDisabled);
+    plasma_set(PlasmaNb, param[PARAM_NB].i);
+    plasma_set(PlasmaIb, param[PARAM_IB].i);
+    plasma_set(PlasmaNumPanelThreads, param[PARAM_MTPF].i);
+
+    //================================================================
+    // Allocate and initialize arrays.
+    //================================================================
+    plasma_complex64_t *A =
+        (plasma_complex64_t*)malloc((size_t)lda*n*sizeof(plasma_complex64_t));
+    assert(A != NULL);
+
+    int *ipiv;
+    ipiv = (int*)malloc((size_t)n*sizeof(int));
+    assert(ipiv != NULL);
+
+    int seed[] = {0, 0, 0, 1};
+    lapack_int retval;
+    retval = LAPACKE_zlarnv(1, seed, (size_t)lda*n, A);
+    assert(retval == 0);
+
+    int zerocol = param[PARAM_ZEROCOL].i;
+    if (zerocol >= 0 && zerocol < n)
+        memset(&A[zerocol*lda], 0, m*sizeof(plasma_complex64_t));
+
+    plasma_complex64_t *Aref;
+    if (test) {
+        Aref = (plasma_complex64_t*)malloc(
+            (size_t)lda*n*sizeof(plasma_complex64_t));
+        assert(Aref != NULL);
+
+        memcpy(Aref, A, (size_t)lda*n*sizeof(plasma_complex64_t));
+    }
+
+    //================================================================
+    // Run and time PLASMA.
+    //================================================================
+    plasma_time_t start = omp_get_wtime();
+    int plainfo = plasma_zgeinv(n, n, A, lda, ipiv);
+    plasma_time_t stop = omp_get_wtime();
+    plasma_time_t time = stop-start;
+
+    param[PARAM_TIME].d = time;
+    param[PARAM_GFLOPS].d = (flops_zgetrf(m, n) + flops_ztrtri(n) + flops_zlauum(n)) / time / 1e9;
+
+    //================================================================
+    // Test results by checking the relative error
+    // ||B - A|| / (||A||)
+    //================================================================
+    if (test) {
+        plasma_complex64_t zmone = -1.0;
+
+        // norm(A)
+        double temp;
+        double Anorm = LAPACKE_zlange_work(
+               LAPACK_COL_MAJOR, 'F', n, n, Aref, lda, &temp);
+
+        int lwork = n;
+        plasma_complex64_t *work =
+            (plasma_complex64_t*)malloc(lwork * sizeof(plasma_complex64_t));
+
+        // B = inv(A)
+		int lapinfo;
+		lapinfo = LAPACKE_zgetrf(CblasColMajor, m, n, Aref, lda, ipiv);
+		if (lapinfo == 0)
+			lapinfo = LAPACKE_zgetri_work(CblasColMajor,
+                                          n, Aref, lda,
+                                          ipiv, work, lwork);
+        if (lapinfo == 0) {
+            // norm(A^{-1})
+            double Inorm = LAPACKE_zlange_work(
+                   LAPACK_COL_MAJOR, 'F', n, n, Aref, lda, &temp);
+
+            // A -= Aref
+            cblas_zaxpy((size_t)lda*n, CBLAS_SADDR(zmone), Aref, 1, A, 1);
+
+            double error = LAPACKE_zlange_work(
+                               LAPACK_COL_MAJOR, 'F', lda, n, A, lda, &temp);
+            if (Anorm*Inorm != 0)
+                error /= (Anorm * Inorm);
+
+            param[PARAM_ERROR].d = error;
+            param[PARAM_SUCCESS].i = error < tol;
+        }
+        else {
+            if (plainfo == lapinfo) {
+                param[PARAM_ERROR].d = 0.0;
+                param[PARAM_SUCCESS].i = 1;
+            }
+            else {
+                param[PARAM_ERROR].d = INFINITY;
+                param[PARAM_SUCCESS].i = 0;
+            }
+        }
+        free(work);
+    }
+
+    //================================================================
+    // Free arrays.
+    //================================================================
+    free(A);
+    free(ipiv);
+    if (test)
+        free(Aref);
+}
