b419ae160451f927ab433ebd90ca8d59f91c3e30
Panruo Wu <pwu11@icl.utk.edu>
2017-07-02 18:22:05 -0400

    zcgbsv.c and test file created; zcgvsv updated with TODOs

---
+++ test/test_zcgbsv.c
@@ -0,0 +1,195 @@
+/**
+ *
+ * @file
+ *
+ *  PLASMA is a software package provided by:
+ *  University of Tennessee, US,
+ *  University of Manchester, UK.
+ *
+ * @precisions mixed zc -> ds
+ *
+ **/
+
+#include "core_blas.h"
+#include "core_lapack.h"
+#include "flops.h"
+#include "plasma.h"
+#include "test.h"
+
+#include <assert.h>
+#include <math.h>
+#include <omp.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define COMPLEX
+
+#define A(i_, j_) A[(i_) + (size_t)lda*(j_)]
+
+/***************************************************************************//**
+ *
+ * @brief Tests ZCPOSV
+ *
+ * @param[in,out] param - array of parameters
+ * @param[in]     run - whether to run test
+ *
+ * Sets flags in param indicating which parameters are used.
+ * If run is true, also runs test and stores output parameters.
+ ******************************************************************************/
+void test_zcgesv(param_value_t param[], bool run)
+{
+    //================================================================
+    // Mark which parameters are used.
+    //================================================================
+    param[PARAM_DIM    ].used = PARAM_USE_N;
+    param[PARAM_NRHS   ].used = true;
+    param[PARAM_PADA   ].used = true;
+    param[PARAM_PADB   ].used = true;
+    param[PARAM_NB     ].used = true;
+    param[PARAM_IB     ].used = true;
+    param[PARAM_MTPF   ].used = true;
+    param[PARAM_ITERSV ].used = true;
+    param[PARAM_ZEROCOL].used = true;
+    if (! run)
+        return;
+
+    //================================================================
+    // Set parameters
+    //================================================================
+    int n    = param[PARAM_DIM].dim.n;
+    int nrhs = param[PARAM_NRHS].i;
+    int lda  = imax(1, n + param[PARAM_PADA].i);
+    int ldb  = imax(1, n + param[PARAM_PADB].i);
+    int ldx  = ldb;
+    int ITER;
+
+    int    test = param[PARAM_TEST].c == 'y';
+    double tol  = param[PARAM_TOL].d * LAPACKE_dlamch('E');
+
+    //================================================================
+    // Set tuning parameters
+    //================================================================
+    plasma_set(PlasmaTuning, PlasmaDisabled);
+    plasma_set(PlasmaNb, param[PARAM_NB].i);
+    plasma_set(PlasmaIb, param[PARAM_IB].i);
+    plasma_set(PlasmaNumPanelThreads, param[PARAM_MTPF].i);
+
+    //================================================================
+    // Allocate and initialize arrays
+    //================================================================
+    plasma_complex64_t *A = (plasma_complex64_t *)malloc(
+        (size_t)lda*n*sizeof(plasma_complex64_t));
+    assert(A != NULL);
+
+    int *ipiv = (int*)malloc((size_t)n*sizeof(int));
+    assert(ipiv != NULL);
+
+    plasma_complex64_t *B = (plasma_complex64_t *)malloc(
+        (size_t)ldb*nrhs*sizeof(plasma_complex64_t));
+    assert(B != NULL);
+
+    plasma_complex64_t *X = (plasma_complex64_t *)malloc(
+        (size_t)ldx*nrhs*sizeof(plasma_complex64_t));
+    assert(X != NULL);
+
+    // Initialize random A
+    int seed[] = {0, 0, 0, 1};
+    lapack_int retval;
+    retval = LAPACKE_zlarnv(1, seed, (size_t)lda*n, A);
+    assert(retval == 0);
+
+    int zerocol = param[PARAM_ZEROCOL].i;
+    if (zerocol >= 0 && zerocol < n)
+        memset(&A[zerocol*lda], 0, n*sizeof(plasma_complex64_t));
+
+    // Initialize B
+    retval = LAPACKE_zlarnv(1, seed, (size_t)ldb*nrhs, B);
+    assert(retval == 0);
+
+    plasma_complex64_t *Aref = NULL;
+    if (test) {
+        Aref = (plasma_complex64_t *)malloc(
+            (size_t)lda*n*sizeof(plasma_complex64_t));
+        assert(Aref != NULL);
+        memcpy(Aref, A, (size_t)lda*n*sizeof(plasma_complex64_t));
+    }
+
+    //================================================================
+    // Run and time PLASMA
+    //================================================================
+    plasma_time_t start = omp_get_wtime();
+    int plainfo = plasma_zcgesv(n, nrhs, A, lda, ipiv, B, ldb, X, ldx, &ITER);
+    plasma_time_t stop = omp_get_wtime();
+    plasma_time_t time = stop-start;
+    double flops = flops_zgetrf(n, n) + flops_zgetrs(n, nrhs);
+    param[PARAM_ITERSV].i = ITER;
+    param[PARAM_TIME].d   = time;
+    param[PARAM_GFLOPS].d = flops / time / 1e9;
+
+    //================================================================
+    // Test results by checking the residual
+    //
+    //                      || B - AX ||_I
+    //                --------------------------- < epsilon
+    //                 || A ||_I * || X ||_I * N
+    //
+    //================================================================
+    if (test) {
+        if (plainfo == 0) {
+            plasma_complex64_t alpha =  1.0;
+            plasma_complex64_t beta  = -1.0;
+
+            lapack_int mtrxLayout = LAPACK_COL_MAJOR;
+            lapack_int mtrxNorm   = 'I';
+
+            double *work = (double *)malloc(n*sizeof(double));
+            assert(work != NULL);
+
+            // Calculate infinite norms of matrices A_ref and X
+            double Anorm = LAPACKE_zlange_work(mtrxLayout, mtrxNorm, n, n, Aref,
+                                               lda, work);
+            double Xnorm = LAPACKE_zlange_work(mtrxLayout, mtrxNorm, n, nrhs, X,
+                                               ldx, work);
+
+            // Calculate residual R = A*X-B, store result in B
+            cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, n, nrhs, n,
+                        CBLAS_SADDR(alpha), Aref, lda,
+                                            X,    ldx,
+                        CBLAS_SADDR(beta),  B,    ldb);
+
+            // Calculate infinite norm of residual matrix R
+            double Rnorm = LAPACKE_zlange_work(mtrxLayout, mtrxNorm, n, nrhs, B,
+                                               ldb, work);
+            // Calculate relative error
+            double residual = Rnorm / ( n*Anorm*Xnorm );
+
+            param[PARAM_ERROR].d   = residual;
+            param[PARAM_SUCCESS].i = residual < tol;
+
+            free(work);
+        }
+        else {
+            int lapinfo = LAPACKE_zcgesv(
+                              LAPACK_COL_MAJOR,
+                              n, nrhs, A, lda, ipiv, B, ldb, X, ldx, &ITER);
+            if (plainfo == lapinfo) {
+                param[PARAM_ERROR].d = 0.0;
+                param[PARAM_SUCCESS].i = 1;
+            }
+            else {
+                param[PARAM_ERROR].d = INFINITY;
+                param[PARAM_SUCCESS].i = 0;
+            }
+        }
+
+        free(Aref);
+    }
+
+    //================================================================
+    // Free arrays
+    //================================================================
+    free(A); free(ipiv); free(B); free(X);
+}
