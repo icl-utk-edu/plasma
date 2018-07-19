607810700c95bb217e7b1e8e767f7e5c11aa16ff
Maksims Abalenkovs <m.abalenkovs@manchester.ac.uk>
2018-07-19 11:36:19 +0300

    * Preprocessor macro to elide OpenMP priorities has been added.

--- include/plasma_internal.h
+++ include/plasma_internal.h
@@ -10,6 +10,10 @@
 #ifndef ICL_PLASMA_INTERNAL_H
 #define ICL_PLASMA_INTERNAL_H
 
+#if ((__GNUC__ == 6) && (__GNUC_MINOR__ < 1)) || (__GNUC__ < 6)
+  #define priority(p)
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 
