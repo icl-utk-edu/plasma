52dae9b1055facd9f0995ad2d848e2cbe63bb2ab
Piotr Luszczek <luszczek@icl.utk.edu>
2018-09-20 16:05:02 -0400

    Set variables to indicate CBLAS wsa found

--- Modules/FindCBLAS.cmake
+++ Modules/FindCBLAS.cmake
@@ -3,12 +3,12 @@
 # FindCBLAS
 # --------
 #
-# Find CBLAS headers and library
+# Find CBLAS header files and libraries
 # This module sets the following variables:
 #
 # ::
 #
-#   CBLAS_FOUND - set to true if a library implementing the BLAS interface
+#   CBLAS_FOUND - set to true if a library implementing the BLAS C interface
 #     is found
 
 find_path(PLASMA_MKL_CBLAS_PATH mkl_cblas.h "$ENV{MKLROOT}" "$ENV{MKLROOT}/include")
@@ -16,10 +16,13 @@ if ( PLASMA_MKL_CBLAS_PATH )
   message( STATUS "Found Intel MKL CBLAS header in ${PLASMA_MKL_CBLAS_PATH}")
   include_directories(${PLASMA_MKL_CBLAS_PATH})
 
+  add_definitions( -DPLASMA_WITH_MKL ) # this adds command line option only
   set( PLASMA_WITH_MKL TRUE )
+
+  set(CBLAS_FOUND TRUE)
 endif ( PLASMA_MKL_CBLAS_PATH )
 
 #include(CheckIncludeFiles)
 #set(CBLAS_MKL_FOUND "FALSE")
-#CHECK_INCLUDE_FILES( mkl_cblas.h;mkl_lapacke.h PLASMA_HAVE_MKL_CBLAS)
+#CHECK_INCLUDE_FILES( mkl_cblas.h;mkl_lapacke.h PLASMA_WITH_MKL_CBLAS)
 #message( STATUS "Result ${CBLAS_MKL_FOUND}")
