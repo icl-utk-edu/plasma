101bc320c8867b9f3d783b832491e73ba9ae4200
Piotr Luszczek <luszczek@icl.utk.edu>
2018-09-26 23:50:11 -0400

    Add version information

--- include/plasma_types.h
+++ include/plasma_types.h
@@ -12,6 +12,14 @@
 
 #include <complex.h>
 
+/*
+ * RELEASE is a, b, c, f (alpha, beta, candidate, and final).
+ * */
+#define PLASMA_VERSION_MAJOR   18
+#define PLASMA_VERSION_MINOR   9
+#define PLASMA_VERSION_MICRO   0
+#define PLASMA_VERSION_RELEASE 'f'
+
 #ifdef __cplusplus
 extern "C" {
 #endif
