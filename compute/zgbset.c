e2646eb25df5309ef8a9a917352a8c48877a509e
Danny Mishler <dsmishler@gmail.com>
2021-12-01 19:37:24 -0800

    minor bug fix: sometimes zgbset would call zlaset with n<0, performing no computation and giving an error message

--- compute/zgbset.c
+++ compute/zgbset.c
@@ -31,11 +31,21 @@ void plasma_zgbset(int m, int n, int kl, int ku,
     if (m == 0 || n == 0)
         return;
 
+    if ((kl < 0) ||
+        (kl > imax(m,n)-1)) {
+        plasma_error("illegal value of kl");
+        return;
+    }
+    if ((ku < 0) ||
+        (ku > imax(m,n)-1)) {
+        plasma_error("illegal value of ku");
+        return;
+    }
     // square matrix OR rectangular tall matrix
     if(m>=n)
     {
         int cornerI, cornerJ, extralower, extraupper;
-        if(kl+ku <= m)
+        if(kl+ku < m) // can be < or <=, the difference is zlaset on m=0
         {
             plasma_zlaset(PlasmaGeneral, m-kl-ku, n-1, 0, 0, pA+1+kl, lda+1);
             extralower = ku-1;
@@ -49,7 +59,8 @@ void plasma_zgbset(int m, int n, int kl, int ku,
         for(; extralower > 0; extralower--)
         {
             cornerI = m-extralower;
-            plasma_zlaset(PlasmaGeneral,1,extralower,0,0,pA+cornerI,lda+1);
+            plasma_zlaset(PlasmaGeneral,1,(extralower < n ? extralower: n),
+                                          0,0,pA+cornerI,lda+1);
         }
         for(; extraupper > 0; extraupper--)
         {
@@ -57,6 +68,7 @@ void plasma_zgbset(int m, int n, int kl, int ku,
             plasma_zlaset(PlasmaGeneral,1,extraupper,0,0,pA+lda*cornerJ,lda+1);
         }
         if(m>n) // m strictly greater
+                // (clears a "tail" on bottom right below diag)
         {
             plasma_zlaset(PlasmaGeneral, (m-n)-kl, 1, 0, 0,
                             pA+(lda*(n-1))+(n+kl), lda);
@@ -67,7 +79,7 @@ void plasma_zgbset(int m, int n, int kl, int ku,
     {
         // clear out a square part of the matrix (left side).
         int cornerI, cornerJ, extralower, extraupper;
-        if(kl+ku <= m)
+        if(kl+ku < m)
         {
             plasma_zlaset(PlasmaGeneral, m-kl-ku, m-1, 0, 0, pA+1+kl, lda+1);
             extralower = ku-1;
@@ -92,6 +104,10 @@ void plasma_zgbset(int m, int n, int kl, int ku,
         for(int i = 0; i < m; i++)
         {
             int blcols = (i-(m-1)+ku) > 0 ? (i-(m-1)+ku) : 0;
+            if (n-(m+blcols) <= 0)
+            {
+               continue;
+            }
             plasma_zlaset(PlasmaGeneral,1,n-(m+blcols),0,0,
                                           pA+(lda*(m+blcols))+i,lda);
         }
