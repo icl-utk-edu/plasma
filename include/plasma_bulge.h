c432ba853111bc2533dfac2e5c5c75fc82bae43e
Mark Gates <Mark.Gates@amd.com>
2026-02-11 20:57:09 -0500

    simplify and document findVTpos code

--- include/plasma_bulge.h
+++ include/plasma_bulge.h
@@ -27,7 +27,7 @@ static inline int plasma_ceildiv( int x, int y )
  *  @param[in] nb
  *  @param[in] Vblksiz
  *  @param[in] sweep
- *  @param[in] st
+ *  @param[in] first
  *  @param[out] Vpos
  *  @param[out] TAUpos
  *  @param[out] Tpos
@@ -35,30 +35,28 @@ static inline int plasma_ceildiv( int x, int y )
  *
  **/
 static inline void findVTpos(
-    int n, int nb, int Vblksiz, int sweep, int st,
-    int *Vpos, int *TAUpos, int *Tpos, int *myblkid)
+    int n, int nb, int Vblksiz, int sweep, int first,
+    int *Vpos, int *TAUpos, int *Tpos, int *blkid)
 {
-    int mastersweep, blkid, locj, ldv;
-
-    int prevcolblknb = 0;
-    int prevblkcnt   = 0;
-    int curcolblknb  = 0;
-
-    int nbprevcolblk = sweep/Vblksiz;
-    for (int prevcolblkid = 0; prevcolblkid < nbprevcolblk; ++prevcolblkid) {
-        mastersweep  = prevcolblkid * Vblksiz;
-        prevcolblknb = plasma_ceildiv( n - (mastersweep + 2), nb );
-        prevblkcnt   = prevblkcnt + prevcolblknb;
+    // Count the number of V blocks in previous block columns.
+    // The left column of a V block has nb rows, except the bottom V block can
+    // have between 2 and nb rows. If there's only 1 row, there's nothing to
+    // eliminate. The n-2 excludes the top and bottom rows.
+    int blkcnt = 0;
+    for (int j = 0; j < sweep - Vblksiz + 1; j += Vblksiz) {
+        blkcnt += plasma_ceildiv( n - 2 - j, nb );
     }
-    curcolblknb = plasma_ceildiv( st - sweep, nb );
-    blkid       = prevblkcnt + curcolblknb - 1;
-    locj        = sweep % Vblksiz;
-    ldv         = nb + Vblksiz - 1;
+    // Add the number of whole V blocks above the current V block in the current
+    // block column.
+    blkcnt += plasma_ceildiv( first - sweep, nb ) - 1;
 
-    *myblkid= blkid;
-    *Vpos   = blkid*Vblksiz*ldv  + locj*ldv + locj;
-    *TAUpos = blkid*Vblksiz + locj;
-    *Tpos   = blkid*Vblksiz*Vblksiz + locj*Vblksiz + locj;
+    int locj = sweep % Vblksiz;   // col and row within this V block.
+    int ldv  = nb + Vblksiz - 1;  // height of V block
+
+    *blkid  = blkcnt;
+    *Vpos   = blkcnt*Vblksiz*ldv + locj*ldv + locj;
+    *TAUpos = blkcnt*Vblksiz + locj;
+    *Tpos   = blkcnt*Vblksiz*Vblksiz + locj*Vblksiz + locj;
 }
 
 /***************************************************************************//**
@@ -74,17 +72,13 @@ static inline void findVTpos(
 static inline void findVTsiz(
     int n, int nb, int Vblksiz, int *blkcnt, int *ldv )
 {
-    int curcolblknb, mastersweep;
-
-    *blkcnt = 0;
-    int nbcolblk = plasma_ceildiv( n - 1, Vblksiz );
-    for (int colblk = 0; colblk < nbcolblk; ++colblk) {
-        mastersweep = colblk * Vblksiz;
-        curcolblknb = plasma_ceildiv( n - (mastersweep + 2), nb );
-        *blkcnt     = *blkcnt + curcolblknb;
+    // Count the number of V blocks in all block columns. See findVTpos.
+    int blkcnt_ = 0;
+    for (int j = 0; j < n - 1; j += Vblksiz) {
+        blkcnt_ += plasma_ceildiv( n - 2 - j, nb );
     }
-    *blkcnt = *blkcnt + 1;
-    *ldv = nb + Vblksiz - 1;
+    *blkcnt = blkcnt_ + 1;
+    *ldv  = nb + Vblksiz - 1;  // height of V block
 }
 
 #endif // PLASMA_BULGE_H
